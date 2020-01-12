37c2180536a4b249fa7062b87d77ee5e6b576fe6
Tylor Trub <Tylor.Trub@project.com>
2020-01-12 17:00:51 -0500

    Commit.

--- FormsVirusTest/Properties/Resources.Designer.cs
+++ FormsVirusTest/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FormsVirusTest.Properties {
+namespace TestForms.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FormsVirusTest.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FormsVirusTest.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("TestForms.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- FormsVirusTest/Properties/Settings.Designer.cs
+++ FormsVirusTest/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FormsVirusTest.Properties {
+namespace TestForms.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
